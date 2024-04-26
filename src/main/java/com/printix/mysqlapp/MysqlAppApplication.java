75997744577e5890697e77b3205288d51f5d4752
Konstantin Tchoumak <Konstantin.Tchoumak@tungstenautomation.com>
2024-04-26 10:24:16 -0400

    initial from https://start.spring.io/

---
+++ src/main/java/com/printix/mysqlapp/MysqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.printix.mysqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/printix/mysqlapp/MysqlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printix.mysqlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
